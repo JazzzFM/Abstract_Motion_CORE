1921c2e23bfecba05ad308011857b3efd0fb999e
Jaziel D. Flores R <jazzzfm@protonmail.com>
2021-08-16 10:06:53 -0500

    Update jobscript

--- job-script.sh
+++ job-script.sh
@@ -10,19 +10,16 @@ echo $SLURM_JOB_ID
 echo $SLURM_PRUEBA
 echo $SLURM_JOB_NUM_NODES
 
-# Load Intel Parallel Studio
 source /opt/rh/devtoolset-7/enable
 source /software/LNS/intel-parallel-studio-xe-2017/bin/compilervars.sh -arch intel64 -platform linux
 source /software/LNS/intel-parallel-studio-xe-2017/impi/2017.4.239/bin64/mpivars.sh
 source /software/LNS/intel-parallel-studio-xe-2017/mkl/bin/mklvars.sh intel64
 
-# Build the code
-mpicxx main.cpp SimplexAbstract.cpp SimplexAlpha.cpp LocalSearch.cpp Matrix.cpp Covering.hpp RCC.hpp -std=c++0x -O3 -mavx  -pthread -o motion 
+mpicxx  main.cpp SimplexAbstract.cpp SimplexAlpha.cpp LocalSearch.cpp Matrix.cpp  Covering.hpp  RCC.hpp -DMPICH_IGNORE_CXX_SEEK  -std=c++0x -O3 -mavx  -pthread -o absMotion-intel-ignore-crecxxseek
 
 date;
 
-# Run de program
-time mpirun -np 96 ./motion > motion.out
+time mpirun -np 96 ./absMotion-intel-ignore-crecxxseek > absMotion-intel-ignore-crecxxseek.out
 
 date;
 
